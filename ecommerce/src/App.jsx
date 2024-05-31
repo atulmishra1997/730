4ae8553df59c3f785d649fdc8feb9f05eacf8b3c
Atul Mishra <atulmishra1997@hotmail.com>
2024-05-31 08:15:51 +0530

    Atul

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -5,7 +5,8 @@ function App() {
 
   return (
     <>
-     <p className=" text-3xl text-center text-black hover:text-red-600">Hello worrld</p>
+    <p className=" text-3xl text-center text-black hover:text-red-600">Hello world</p>
+    <h1 className=" text-3xl text-center text-black hover:text-red-600">This is Atul Mishra</h1>
     </>
   )
 }
